35b75dc7cd671833e66032dc3dcecf8cda3cf02e
ramirami123 <146880496+ramirami123@users.noreply.github.com>
2024-10-10 16:52:17 -0300

    28

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,34 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Curiosidade sobre gatos',
+    'Quantos filhotes de gatos uma gata pode dar?',
+    'Um par de gatos pode dar origem a mais de 420.000 gatos em apenas 7 anos'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Curiosidades sobre gatos',
+    'Porque os gatos tem o costume de afofar as cobertas?',
+    'Por uma lembranca do que faziam quando filhotes durante a amamentacao'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Curiosidades sobre gatos',
+    'Quantos musculos os gatos possuim no ouvido?',
+    'Os gatos possuim 33 músculos que controlam o ouvido externo'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Curiosidades sobre gatos',
+    'Quanto um gato pode saltar?',
+    'Em um unico salto, o gato é capaz de pular 5 vezes a sua altura'
+)
+criaCartao(
+    'Curiosidades sobre gatos',
+    'Qual é a diferença entre o gato e o cachorro?',
+    ' Diferente dos cachorros o gato costuma manter a cabeça sempre abaixada ao perseguir uma presa,'
+)
+
+criaCartao(
+    'Curiosidades sobre gatos',
+    ' Quantos gatos uma gata femea pode dar?',
+    'Uma gata femea e capaz de dar a luz a uma media de 9 gatos filhotes'
 )
